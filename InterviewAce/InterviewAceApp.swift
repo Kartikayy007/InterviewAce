ab25c52eed6070413f82678488aec750d7ce77b7
kartikayy007 <kartikayy6969@gmail.com>
2025-04-29 14:33:51 +0530

    Initial Commit

---
+++ InterviewAce/InterviewAceApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewAceApp.swift
+//  InterviewAce
+//
+//  Created by kartikay on 29/04/25.
+//
+
+import SwiftUI
+
+@main
+struct InterviewAceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
